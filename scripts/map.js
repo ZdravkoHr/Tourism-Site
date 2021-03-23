027b131ebe5b645901c3e20585fee866bdee21cd
Zdravko Hristov <zdravkohristov0@gmail.com>
2021-03-23 16:33:47 +0200

    created basic map

---
+++ scripts/map.js
@@ -0,0 +1,12 @@
+var map = L.map('map').setView([42.1267995, 25.221695], 13);
+// L.tileLayer(
+// 	'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
+// 	{
+// 		tileSize: 512,
+// 		zoomOffset: -1,
+// 		minZoom: 1,
+// 		attribution:
+// 			'\u003ca href="https://www.maptiler.com/copyright/" target="_blank"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href="https://www.openstreetmap.org/copyright" target="_blank"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e',
+// 		crossOrigin: true,
+// 	}
+// ).addTo(map);
